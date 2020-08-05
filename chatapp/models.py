092b5a4db8dd37ff5267f8abf03b2692f785c8de
udit5656 <uditgarg5656@gmail.com>
2020-08-06 00:47:11 +0530

    First Commit

---
+++ chatapp/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+from datetime import datetime
+
+
+# Create your models here.
+class Message(models.Model):
+    msg_text = models.CharField(max_length=200, blank=False)
+    send_time = models.DateTimeField(default=datetime.now)
+    sender = models.ForeignKey(User, related_name='sender_user', on_delete=models.CASCADE)
+    reciever = models.ForeignKey(User, related_name='reciever_user', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.msg_text[:10]
