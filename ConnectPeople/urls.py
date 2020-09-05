22e50783df197b108c6e0035c3a38564752fff39
udit5656 <uditgarg5656@gmail.com>
2020-09-05 14:15:18 +0530

    Minor changes

--- ConnectPeople/urls.py
+++ ConnectPeople/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='accounts/login/')),
     path('admin/', admin.site.urls),
     path('chatapp/', include('chatapp.urls')),
     path('accounts/', include('accounts.urls'))
