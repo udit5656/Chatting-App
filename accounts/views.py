c2dd4586e3a7e7f4c1448e0003a481854816f3b4
udit5656 <uditgarg5656@gmail.com>
2020-09-07 01:34:49 +0530

    Fixed Login Page bug

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
@@ -15,7 +16,8 @@ def login_user(request):
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse('chatapp:home'))
-        return render(request, 'registration/login.html')
+        messages.add_message(request, messages.INFO, 'Wrong Credentials')
+        return HttpResponseRedirect(reverse('accounts:login'))
     return render(request, 'registration/login.html')
 
 
